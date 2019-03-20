6166899c5a32aa0bdb74fe74ea4bc9c36656a734
Unknown <massive.annas@gmail.com>
2019-03-20 10:53:06 +0700

    revisi

--- soal4/soal4.c
+++ soal4/soal4.c
@@ -35,8 +35,30 @@ int main(){
         close(STDOUT_FILENO);
         close(STDERR_FILENO);
     
+        time_t now;
+        double diff;
+        struct stat st;
+        char* file = "/home/vagrant/Documents/makanan/makan_enak.txt";
+        char* new;
+        int count = 0;
+        FILE* sehat;
+
         while(1){
-    
+            if(stat(file, &st) == -1){
+                perror("fail");
+                exit(EXIT_FAILURE);
+            }
+            else{
+                time(&now);
+                diff = difftime(now, st.st_atime);
+
+                if(diff <= 30 ){
+                    sprintf(new,"/home/vagrant/Documents/makanan/makan_sehat%d.txt", ++count);
+                    
+                    sehat = fopen(new, "w+");
+                }
+            }
+            sleep(5);
         }
         
         exit(EXIT_SUCCESS);
